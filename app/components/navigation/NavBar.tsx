a1331242e4bb33ed5af8e4febaf0630f0ee0169f
jmwalsh91 <jordanmwalsh@gmail.com>
2022-03-17 17:45:44 -0600

    removed nested a tags, addressed errors

--- app/components/navigation/NavBar.tsx
+++ app/components/navigation/NavBar.tsx
@@ -10,20 +10,7 @@ function NavBar({}: Props) {
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+    
             </label>
             <ul
               tabIndex={0}
@@ -59,7 +46,7 @@ function NavBar({}: Props) {
               </li>
             </ul>
           </div>
-          <Link to="/"><a className="btn btn-ghost normal-case text-2xl">LV8R</a></Link>
+          <Link to="/"><div className="btn btn-ghost normal-case text-2xl">LV8R</div></Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
